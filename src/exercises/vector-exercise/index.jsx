45660ca39df9ee6e7d1422d94423d782ee7b543e
Mike Hansbrook <35354129+PDXmjb@users.noreply.github.com>
2026-05-01 09:13:56 -0700

    (fix) continuing game adjustments

--- src/exercises/vector-exercise/index.jsx
+++ src/exercises/vector-exercise/index.jsx
@@ -1,118 +1,126 @@
 import { useState } from 'react';
 import './styles.scss';
 
-function VectorExercise() {
-  const buildBoard = (width) => {
-    const column = [];
-    for (let i = 0; i < width; i++) {
-      const row = [];
+function buildBoard(width) {
+  return Array.from({ length: width }, () => Array(width).fill('-'));
+}
 
-      for (let j = 0; j < width; j++) {
-        row.push('-');
-      }
+function noEmptySpaces(currentBoard) {
+  const fullBoard = currentBoard.flat(Infinity);
+  return !fullBoard.includes('-');
+}
 
-      column.push(row);
+function determineWin(currentBoard, width) {
+  // Win - all of a row is the same turn value (X or O)
+  // OR - all of a column is the same turn value
+  // OR - all of a diagonal left/right are the same value
+
+  const MAX_ARR_LENGTH = width - 1;
+
+  // Check Rows
+  for (let i = 0; i < width; i++) {
+    const firstRowVal =
+      currentBoard[i][0] !== '-' ? currentBoard[i][0] : 'SKIP';
+
+    if (currentBoard[i].every((v) => v === firstRowVal)) {
+      return true;
     }
-    return column;
-  };
+  }
 
+  // Check columns
+  for (let i = 0; i < width; i++) {
+    const firstColVal =
+      currentBoard[0][i] !== '-' ? currentBoard[0][i] : 'SKIP';
+    for (let j = 0; j < width; j++) {
+      if (firstColVal === 'SKIP' || currentBoard[j][i] !== firstColVal) {
+        break;
+      }
+      // If we got to the end, we have a winner!
+      if (j === MAX_ARR_LENGTH && currentBoard[j][i] === firstColVal) {
+        return true;
+      }
+    }
+  }
+
+  // Left diagonal
+  let firstDiagVal = currentBoard[0][0] !== '-' ? currentBoard[0][0] : 'SKIP';
+  for (let i = 0; i < width; i++) {
+    if (firstDiagVal === 'SKIP' || currentBoard[i][i] !== firstDiagVal) {
+      break;
+    }
+    if (i === MAX_ARR_LENGTH && currentBoard[i][i] === firstDiagVal) {
+      return true;
+    }
+  }
+
+  // Right diagonal..., i.e., 0,2; 1,1; 2,0 for a 3x3 board
+  firstDiagVal =
+    currentBoard[MAX_ARR_LENGTH][0] !== '-'
+      ? currentBoard[MAX_ARR_LENGTH][0]
+      : 'SKIP';
+
+  for (let i = 0; i < width; i++) {
+    if (
+      firstDiagVal === 'SKIP' ||
+      currentBoard[i][MAX_ARR_LENGTH - i] !== firstDiagVal
+    ) {
+      break;
+    }
+    if (
+      i === MAX_ARR_LENGTH &&
+      currentBoard[i][MAX_ARR_LENGTH - i] === firstDiagVal
+    ) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function VectorExercise() {
   const [turn, setTurn] = useState('X');
   const [width, setWidth] = useState(3);
   const [winner, setWinner] = useState(null);
+  const [catsGame, setCatsGame] = useState(null);
   const [board, setBoard] = useState(buildBoard(width));
 
-  const resetGame = (width) => {
+  // Handle resetting the game.
+  const resetGameHandler = (width) => {
     setBoard(buildBoard(width));
     setTurn('X');
     setWinner(null);
+    setCatsGame(null);
   };
 
-  const changeWidth = (width) => {
+  // Handle changing the width of the board.
+  const changeWidthHandler = (width) => {
     setWidth(width);
-    resetGame(width);
+    resetGameHandler(width);
   };
 
-  const determineWin = (currentBoard) => {
-    // Win - all of a row is the same turn value (X or O)
-    // OR - all of a column is the same turn value
-    // OR - all of a diagonal left/right are the same value
-
-    // Check Rows
-    for (let i = 0; i < width; i++) {
-      const firstRowVal =
-        currentBoard[i][0] !== '-' ? currentBoard[i][0] : 'SKIP';
-      for (let j = 0; j < width; j++) {
-        if (firstRowVal === 'SKIP' || currentBoard[i][j] !== firstRowVal) {
-          break;
-        }
-        // If we got to the end, we have a winner!
-        if (j === width - 1 && currentBoard[i][j] === firstRowVal) {
-          return true;
-        }
-      }
-    }
-
-    // Check columns
-    for (let i = 0; i < width; i++) {
-      const firstColVal =
-        currentBoard[0][i] !== '-' ? currentBoard[0][i] : 'SKIP';
-      for (let j = 0; j < width; j++) {
-        if (firstColVal === 'SKIP' || currentBoard[j][i] !== firstColVal) {
-          break;
-        }
-        // If we got to the end, we have a winner!
-        if (j === width - 1 && currentBoard[j][i] === firstColVal) {
-          return true;
-        }
-      }
-    }
-
-    // Left diagonal
-    let firstDiagVal = currentBoard[0][0] !== '-' ? currentBoard[0][0] : 'SKIP';
-    for (let i = 0; i < width; i++) {
-      if (firstDiagVal === 'SKIP' || currentBoard[i][i] !== firstDiagVal) {
-        break;
-      }
-      if (i === width - 1 && currentBoard[i][i] === firstDiagVal) {
-        return true;
-      }
-    }
-
-    const max = width - 1;
-    // Right diagonal..., i.e., 0,2; 1,1; 2,0 for a 3x3 board
-    firstDiagVal = currentBoard[max][0] !== '-' ? currentBoard[max][0] : 'SKIP';
-    for (let i = 0; i < width; i++) {
-      if (
-        firstDiagVal === 'SKIP' ||
-        currentBoard[i][max - i] !== firstDiagVal
-      ) {
-        break;
-      }
-      if (i === max && currentBoard[i][max - i] === firstDiagVal) {
-        return true;
-      }
-    }
-
-    return false;
-  };
-
+  // Handle clicking on a game button.
   const clickHandler = (row, column) => {
     const newBoard = [...board];
 
+    // If they clicked on a previously selected space, ignore it.
     if (newBoard[row][column] !== '-') {
       return;
     }
 
-    // Otherwise:
+    // Set the space to be X or O.
     newBoard[row][column] = turn;
 
     // Did someone win or are there no more spaces? If yes, end the game.
-    if (determineWin(newBoard)) {
+    if (determineWin(newBoard, width)) {
       setWinner(turn);
+    } else if (noEmptySpaces(newBoard)) {
+      setCatsGame(true);
     }
 
+    // Otherwise, show the new board state.
     setBoard(newBoard);
 
+    // Change the turn from X -> O or O -> X.
     setTurn(turn === 'X' ? 'O' : 'X');
   };
 
@@ -123,32 +131,40 @@ function VectorExercise() {
       </header>
       <section className="interview__card">
         <div className="interview__controls">
-          <label htmlFor="width">Width: </label>
+          <label htmlFor="width">Game width: </label>
           <input
+            a
             defaultValue={width}
             id="width"
             max={9}
             min={3}
             type="number"
-            onChange={(e) => changeWidth(e.target.value)}
+            onChange={(e) => changeWidthHandler(parseInt(e.target.value, 10))}
           ></input>
-          {!winner && <p>Current turn: {turn}</p>}
+          <button
+            className="restart__button button-positive"
+            type="button"
+            onClick={() => resetGameHandler(width)}
+          >
+            Restart
+          </button>
         </div>
-        {winner && (
-          <>
-            <p>We have a winner! It is {winner}!</p>
-            <button type="button" onClick={() => resetGame(width)}>
-              Restart
-            </button>
-          </>
+        {winner && <p>We have a winner! It is {winner}!</p>}
+        {catsGame && <p>No one wins! Try again.</p>}
+
+        {!winner && (
+          <p>
+            Current turn: <strong>{turn}</strong>
+          </p>
         )}
         {board.map((row, rowIndex) => {
           return (
-            <div key={rowIndex * 10000}>
+            <div key={(rowIndex + 1) * 1000}>
               {row.map((column, colIndex) => {
                 return (
                   <button
-                    disabled={winner}
+                    className="game__button "
+                    disabled={winner || catsGame}
                     key={(colIndex + 1) * 10}
                     type="button"
                     onClick={() => clickHandler(rowIndex, colIndex)}
